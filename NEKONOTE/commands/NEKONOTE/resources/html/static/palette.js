08eb3967945458fd4f2ade5fa36a5024b8bfd41c
kantoku <kandennti@hotmail.com>
2022-09-06 16:44:35 +0900

    javascript refactoring

--- NEKONOTE/commands/NEKONOTE/resources/html/static/palette.js
+++ NEKONOTE/commands/NEKONOTE/resources/html/static/palette.js
@@ -25,6 +25,72 @@ const BTN_ICONS = {
     "Construction": '<i class="bi bi-square"></i>',
 };
 
+const SCOPE_ALL_ID = "scope_all";
+const SCOPE_ACTIVE_ID = "scope_active";
+const SCOPE_CHILDREN_ID = "scope_children";
+
+document.addEventListener("DOMContentLoaded", () => {
+    let adskWaiter = setInterval(() => {
+        dumpLog("DOMContentLoaded");
+        if (window.adsk) {
+            dumpLog("adsk ok");
+            clearInterval(adskWaiter);
+
+            adsk
+            .fusionSendData("DOMContentLoaded", "{}")
+            .then((data) => {
+                const button_group = document.getElementById("button_group");
+                dumpLog(data)
+                let button_info = JSON.parse(data)
+
+                // button
+                const row = init_Buttons(button_info);
+                button_group.appendChild(row);
+
+                // スコープ多言語化していない
+                // scope all
+                const scope_all = initRadio(SCOPE_ALL_ID, "全体", button_group);
+                scope_all.checked=true;
+                scope_all.addEventListener(`change`, function(){
+                    let checkbox = document.getElementById(SCOPE_CHILDREN_ID)
+                    checkbox.disabled = scope_all.checked
+                });
+
+                // scope active
+                const scope_active = initRadio(SCOPE_ACTIVE_ID, "アクティブ", button_group);
+                scope_active.addEventListener(`change`, function(){
+                    let checkbox = document.getElementById(SCOPE_CHILDREN_ID)
+                    checkbox.disabled = !scope_active.checked
+                });
+
+                // scope children
+                const scope_children = initCheck(SCOPE_CHILDREN_ID, "子も含む", button_group)
+                scope_children.checked = true
+            });
+        }
+    }, 100);
+});
+
+window.fusionJavaScriptHandler = {
+    handle: function (action, data) {
+        try {
+            if (action === "command_event") {
+                let values = JSON.parse(data);
+                dumpLog(values['value']);
+                setDisabled(toBoolean(values["value"]));
+            } else if (action === "debugger") {
+                debugger;
+            } else {
+                return `Unexpected command type: ${action}`;
+            }
+        } catch (e) {
+            console.log(e);
+            console.log(`Exception caught with command: ${action}, data: ${data}`);
+        }
+        return "OK";
+    },
+};
+
 function init_Buttons(button_info) {
     // div
     const row = document.createElement("div");
@@ -63,129 +129,61 @@ function init_Buttons(button_info) {
     return row;
 }
 
-// function name(params) {
-//     const div_scope1 = document.createElement("div");
-//     div_scope1.setAttribute("class", "form-check form-check-inline");
-//     div_scope1.setAttribute("id", "scope_all");
-//     button_group.appendChild(div_scope1);
+function initRadio(id, text, parent) {
+    // div
+    const div_scope = document.createElement("div");
+    div_scope.setAttribute("class", "form-check form-check-inline");
+    div_scope.setAttribute("id", id + "div");
+    parent.appendChild(div_scope);
 
-//     const scope_radio1 = document.createElement("input");
-//     scope_radio1.setAttribute("class", "form-check-input");
-//     scope_radio1.setAttribute("type", "radio");
-//     scope_radio1.setAttribute("name", "flexRadioScope");
-//     scope_radio1.setAttribute("id", "flexRadioDefault1");
-//     scope_radio1.setAttribute("value", "option1");
-//     scope_radio1.addEventListener(`change`, function(){
-//         let checkbox = document.getElementById("flexCheckDefault")
-//         checkbox.disabled = scope_radio1.checked
-//     });
-//     scope_radio1.checked=true;
-//     div_scope1.appendChild(scope_radio1);
+    // radio
+    const scope_radio = document.createElement("input");
+    scope_radio.setAttribute("class", "form-check-input");
+    scope_radio.setAttribute("type", "radio");
+    scope_radio.setAttribute("name", "flexRadioScope");
+    scope_radio.setAttribute("id", id);
+    div_scope.appendChild(scope_radio);
 
-//     const scope_label1 = document.createElement("label");
-//     scope_label1.setAttribute("class", "form-check-label");
-//     scope_label1.setAttribute("for", "flexRadioDefault1");
-//     scope_label1.appendChild(document.createTextNode("全体"));
-//     div_scope1.appendChild(scope_label1);
-// }
+    // label
+    const scope_label = document.createElement("label");
+    scope_label.setAttribute("class", "form-check-label");
+    scope_label.setAttribute("for", id);
+    scope_label.appendChild(document.createTextNode(text));
+    div_scope.appendChild(scope_label);
 
+    return scope_radio
+}
 
+function initCheck(id, text, parent) {
+    // div
+    const div_scope = document.createElement("div");
+    div_scope.setAttribute("class", "form-check form-check-inline");
+    div_scope.setAttribute("id", id + "div");
+    parent.appendChild(div_scope);
 
-document.addEventListener("DOMContentLoaded", () => {
-    let adskWaiter = setInterval(() => {
-        dumpLog("DOMContentLoaded");
-        if (window.adsk) {
-            dumpLog("adsk ok");
-            clearInterval(adskWaiter);
+    // checkbox
+    const scope_children = document.createElement("input");
+    scope_children.setAttribute("class", "form-check-input");
+    scope_children.setAttribute("id", id);
+    scope_children.setAttribute("type", "checkbox");
+    scope_children.setAttribute("value", "");
+    scope_children.setAttribute("disabled", "true");
+    div_scope.appendChild(scope_children);
 
-            adsk
-            .fusionSendData("DOMContentLoaded", "{}")
-            .then((data) => {
-                const button_group = document.getElementById("button_group");
-                dumpLog(data)
-                let button_info = JSON.parse(data)
+    // label
+    const label_children = document.createElement("label");
+    label_children.setAttribute("class", "form-check-label");
+    label_children.setAttribute("for", id);
+    label_children.appendChild(document.createTextNode(text));
+    div_scope.appendChild(label_children);
 
-                // button
-                const row = init_Buttons(button_info)
-                button_group.appendChild(row);
-
-                // scope
-                const div_scope1 = document.createElement("div");
-                div_scope1.setAttribute("class", "form-check form-check-inline");
-                div_scope1.setAttribute("id", "scope_all");
-                button_group.appendChild(div_scope1);
-
-                const scope_radio1 = document.createElement("input");
-                scope_radio1.setAttribute("class", "form-check-input");
-                scope_radio1.setAttribute("type", "radio");
-                scope_radio1.setAttribute("name", "flexRadioScope");
-                scope_radio1.setAttribute("id", "flexRadioDefault1");
-                // scope_radio1.setAttribute("value", "option1");
-                scope_radio1.addEventListener(`change`, function(){
-                    let checkbox = document.getElementById("flexCheckDefault")
-                    checkbox.disabled = scope_radio1.checked
-                });
-                scope_radio1.checked=true;
-                div_scope1.appendChild(scope_radio1);
-
-                const scope_label1 = document.createElement("label");
-                scope_label1.setAttribute("class", "form-check-label");
-                scope_label1.setAttribute("for", "flexRadioDefault1");
-                scope_label1.appendChild(document.createTextNode("全体"));
-                div_scope1.appendChild(scope_label1);
-
-                const div_scope2 = document.createElement("div");
-                div_scope2.setAttribute("class", "form-check form-check-inline");
-                div_scope2.setAttribute("id", "scope_active");
-                button_group.appendChild(div_scope2);
-
-                const scope_radio2 = document.createElement("input");
-                scope_radio2.setAttribute("class", "form-check-input");
-                scope_radio2.setAttribute("type", "radio");
-                scope_radio2.setAttribute("name", "flexRadioScope");
-                scope_radio2.setAttribute("id", "flexRadioDefault2");
-                // scope_radio2.setAttribute("value", "option2");
-                scope_radio2.addEventListener(`change`, function(){
-                    let checkbox = document.getElementById("flexCheckDefault")
-                    checkbox.disabled = !scope_radio2.checked
-                });
-                div_scope2.appendChild(scope_radio2);
-
-                const scope_label2 = document.createElement("label");
-                scope_label2.setAttribute("class", "form-check-label");
-                scope_label2.setAttribute("for", "flexRadioDefault2");
-                scope_label2.appendChild(document.createTextNode("アクティブ"));
-                div_scope2.appendChild(scope_label2);
-
-                const div_scope3 = document.createElement("div");
-                div_scope3.setAttribute("class", "form-check form-check-inline");
-                div_scope3.setAttribute("id", "scope_checkbox");
-                button_group.appendChild(div_scope3);
-
-                const scope_children = document.createElement("input");
-                scope_children.setAttribute("class", "form-check-input");
-                scope_children.setAttribute("id", "flexCheckDefault");
-                scope_children.setAttribute("type", "checkbox");
-                scope_children.setAttribute("value", "");
-                scope_children.setAttribute("disabled", "true");
-                div_scope3.appendChild(scope_children);
-
-                const label_children = document.createElement("label");
-                label_children.setAttribute("class", "form-check-label");
-                label_children.setAttribute("for", "flexCheckDefault");
-                label_children.appendChild(document.createTextNode("子も含む"));
-                div_scope3.appendChild(label_children);
-
-            });
-        }
-    }, 100);
-});
-
+    return scope_children
+}
 
 function getScopeValue() {
-    const scope_radio2 = document.getElementById("flexRadioDefault2")
-    if (scope_radio2.checked) {
-        const scope_children = document.getElementById("flexCheckDefault")
+    const scope_active = document.getElementById(SCOPE_ACTIVE_ID)
+    if (scope_active.checked) {
+        const scope_children = document.getElementById(SCOPE_CHILDREN_ID)
         if (scope_children.checked) {
             return "CHILDREN"
         } else {
@@ -194,11 +192,6 @@ function getScopeValue() {
     } else {
         return "ALL"
     }
-}
-
-function setScopeCheckboxDisabled(value) {
-    let checkbox = document.getElementById("scope_checkbox")
-    checkbox.disabled = value
 }
 
 function setDisabled(value) {
@@ -218,23 +211,3 @@ function dumpLog(msg) {
         console.log(msg);
     }
 }
-
-window.fusionJavaScriptHandler = {
-    handle: function (action, data) {
-        try {
-            if (action === "command_event") {
-                let values = JSON.parse(data);
-                dumpLog(values['value']);
-                setDisabled(toBoolean(values["value"]));
-            } else if (action === "debugger") {
-                debugger;
-            } else {
-                return `Unexpected command type: ${action}`;
-            }
-        } catch (e) {
-            console.log(e);
-            console.log(`Exception caught with command: ${action}, data: ${data}`);
-        }
-        return "OK";
-    },
-};
